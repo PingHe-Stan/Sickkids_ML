a85adcf4d35aa690e0ee80d8ea092509513d5a55
Stan <stan@RT5801ML-GGB.local>
2021-10-21 13:20:39 -0400

    src added

---
+++ src/nbconf.py
@@ -0,0 +1,8 @@
+# Preferences of autoformatting & Multiple Output
+from IPython.core.interactiveshell import InteractiveShell
+
+InteractiveShell.ast_node_interactivity = "all"
+
+import warnings
+
+warnings.filterwarnings("ignore")
