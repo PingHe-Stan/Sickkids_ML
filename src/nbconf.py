c10ee15f6682390cfc9322e84467ea2c3382011a
PingHe-Stan <54294815+PingHe-Stan@users.noreply.github.com>
2023-03-02 12:17:36 -0500

    Delete nbconf.py

--- src/nbconf.py
+++
@@ -1,8 +0,0 @@
-# Preferences of autoformatting & Multiple Output
-from IPython.core.interactiveshell import InteractiveShell
-
-InteractiveShell.ast_node_interactivity = "all"
-
-import warnings
-
-warnings.filterwarnings("ignore")
